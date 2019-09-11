07de8c676073d8caf0f3f51c5d77cb7270104748
zyk <yjjc1996@163.com>
2019-09-11 23:07:16 +0800

    添加了一个SayHello

---
+++ src/main/java/com/yk/hello/SayHello.java
@@ -0,0 +1,10 @@
+package com.yk.hello;
+
+/**
+ * created by 不器 on 2019/9/11.
+ */
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
